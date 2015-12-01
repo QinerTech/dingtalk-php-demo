70fe77f2bc755b01abe2f783fe46ee3b63e1b071
flashoop <5641817@qq.com>
2015-12-01 22:11:32 +0800

    Update env.php

--- env.php
+++ env.php
@@ -8,7 +8,7 @@ define("SECRET", "");
 
 //Suite
 define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");
-define("SUITE_KEY", "suitebjpxj4rxhluc26pg");
+define("SUITE_KEY", "suiteiqyslva2c9sfysh6");
 define("SUITE_SECRET", "I-w-LeK48lcArAorM1aw-qV63mT9obe-SpBDx7IhFsKqlrQjcykvBPKNvqt9zIj3");
 define("TOKEN", "flash123");
 define("ENCODING_AES_KEY", "9z5dofwm6lxjdov499hj2f6150h7vvaxux65sl17v8x");
